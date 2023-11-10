094cdbf360c981d07b72cdd8009249f9afcb5b9c
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-11-10 17:02:18 +0100

    Finish left side and starting booking design side

--- app/javascript/controllers/description_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="description"
-export default class extends Controller {
-  static targets = ["modal"]
-  openModal() {
-    console.log(this.modalTarget)
-  }
-}
