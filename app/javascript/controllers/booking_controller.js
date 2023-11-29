43cc0fb8aadc16d97c3d4855de0e5b2083c99769
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-11-29 12:01:40 +0100

    Adding flatpickr to card-booking and redesign this. Managing for price, number of night, total price + cleaning price

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -5,13 +5,31 @@ export default class extends Controller {
   static values = {
     price: Number
   }
-  static targets = ["price", "startDate", "endDate"]
+  static targets = ["price", "startDate", "endDate", "priceValue", "taxe", "total", "night"]
+  connect () {
+    this.taxeTarget.innerText = (this.priceValue * 0.18).toFixed()
+    this.totalTarget.innerText = this.priceValue + Number(this.taxeTarget.innerText) + 10
+    this.nightTarget.innerText =  "".concat(this.priceValue, ' ', "€ x 1 Nuit")
+    console.log(this.nightTarget.innerText)
+  }
 
   updatePrice() {
     const start = Date.parse(this.startDateTarget.value)
     const end = Date.parse(this.endDateTarget.value)
-    const diffInDay = (new Date(end) - new Date(start)) / (1000 * 60 * 60 * 24)
-    this.priceTarget.value = diffInDay * this.priceValue
-
+    if (start > end || start === end) {
+      this.endDateTarget.value = ""
+      this.priceTarget.innerText = this.priceValue
+      this.priceValueTarget.value = this.priceValue
+      this.taxeTarget.innerText = (this.priceValue * 0.18).toFixed()
+      this.totalTarget.innerText = this.priceValue + Number(this.taxeTarget.innerText) + 10 
+    } 
+    else {
+      const diffInDay = (new Date(end) - new Date(start)) / (1000 * 60 * 60 * 24)
+      this.priceTarget.innerText = diffInDay * this.priceValue
+      this.priceValueTarget.value = diffInDay * this.priceValue
+      this.taxeTarget.innerText = (Number(this.priceValueTarget.value) * 0.18).toFixed()
+      this.totalTarget.innerText =  Number(this.priceValueTarget.value) + Number(this.taxeTarget.innerText) + 10
+      this.nightTarget.innerText = "".concat(this.priceValue, ' ', "€ x",' ', diffInDay, ' ', "Nuits")
+    }
   }
 }
